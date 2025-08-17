98436dd3bc3c2d7f607fc6cbc0f803f3307e5d0d
mihirshah2005 <e1427144@u.nus.edu>
2025-08-17 18:28:46 +0800

    Level-1: implement Echo (Shahzam repeats input until 'bye')

--- src/main/java/Shahzam.java
+++ src/main/java/Shahzam.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
+
 public class Shahzam {
+
+    private final String WELCOME_MSG = "\nThe word was spoken, SHAHZAM awakens.\n" + "What can I do for you today?";
+    private final String EXIT_MSG = "Thunder quiets. SHAHZAM signing off, until next time.";
+    private final String logo =  "  ____  _           _                        \n" +
+            " / ___|| |__   __ _| |__  ______ _ _ __ ___  \n" +
+            " \\___ \\| '_ \\ / _` | '_ \\|_  / _` | '_ ` _ \\ \n" +
+            "  ___) | | | | (_| | | | |/ / (_| | | | | | |\n" +
+            " |____/|_| |_|\\__,_|_| |_/___\\__,_|_| |_| |_|\n" +
+            "                                             ";
+
+
+    public void run() {
+        String input;
+        System.out.println(logo + WELCOME_MSG);
+
+        Scanner sc = new Scanner(System.in);
+        while (!(input = sc.nextLine()).equals("bye")) {
+            System.out.println(input);
+        }
+        sc.close();
+
+        System.out.println(EXIT_MSG);
+    }
+
     public static void main(String[] args) {
-        String WELCOME_MSG = "\nThe word was spoken, SHAHZAM awakens.\n" + "What can I do for you today?";
-        String EXIT_MSG = "Thunder quiets. SHAHZAM signing off, until next time.";
-
-        String logo =  "  ____  _           _                        \n" +
-                " / ___|| |__   __ _| |__  ______ _ _ __ ___  \n" +
-                " \\___ \\| '_ \\ / _` | '_ \\|_  / _` | '_ ` _ \\ \n" +
-                "  ___) | | | | (_| | | | |/ / (_| | | | | | |\n" +
-                " |____/|_| |_|\\__,_|_| |_/___\\__,_|_| |_| |_|\n" +
-                "                                             ";
-
-                System.out.println(logo + WELCOME_MSG);
-
-                System.out.println(EXIT_MSG);
+        new Shahzam().run();
     }
 }
