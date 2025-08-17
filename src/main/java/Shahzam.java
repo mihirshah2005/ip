7c47cc445865d2f06ca51b04062251c189064615
mihirshah2005 <e1427144@u.nus.edu>
2025-08-17 18:17:47 +0800

    Level-0: rename to Shahzam; add welcome/exit messages

---
+++ src/main/java/Shahzam.java
@@ -0,0 +1,17 @@
+public class Shahzam {
+    public static void main(String[] args) {
+        String WELCOME_MSG = "\nThe word was spoken, SHAHZAM awakens.\n" + "What can I do for you today?";
+        String EXIT_MSG = "Thunder quiets. SHAHZAM signing off, until next time.";
+
+        String logo =  "  ____  _           _                        \n" +
+                " / ___|| |__   __ _| |__  ______ _ _ __ ___  \n" +
+                " \\___ \\| '_ \\ / _` | '_ \\|_  / _` | '_ ` _ \\ \n" +
+                "  ___) | | | | (_| | | | |/ / (_| | | | | | |\n" +
+                " |____/|_| |_|\\__,_|_| |_/___\\__,_|_| |_| |_|\n" +
+                "                                             ";
+
+                System.out.println(logo + WELCOME_MSG);
+
+                System.out.println(EXIT_MSG);
+    }
+}
