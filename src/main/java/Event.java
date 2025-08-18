a9843c85c49b7fc37a5b94b27bde6185d8f6d839
mihirshah2005 <e1427144@u.nus.edu>
2025-08-18 23:28:37 +0800

    Updated Event.java to handle 'from' and 'to' times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
 public class Event extends Task {
     private String time;
+    private String fromTime;
+    private String toTime;
 
-    public Event(String description, String time) {
+    public Event(String description, String fromTime, String toTime) {
         super(description);
-        this.time = time;
+        this.fromTime = fromTime;
+        this.toTime = toTime;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + time + ")";
+        return "[E]" + super.toString() + " (from: " + fromTime + " to: " + toTime + ")";
     }
 }
