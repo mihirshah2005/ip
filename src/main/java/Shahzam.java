a74c8c7e95047e0a0939f190d439f85b5f4cd673
mihirshah2005 <e1427144@u.nus.edu>
2025-08-18 23:30:11 +0800

    Level-4: Added ToDo, Event, and Deadline tasks

--- src/main/java/Shahzam.java
+++ src/main/java/Shahzam.java
@@ -32,6 +32,12 @@ public class Shahzam {
                 TaskDone(input);
             } else if (input.startsWith("unmark ")) {
                 TaskUnmark(input);
+            } else if (input.startsWith("todo ")) {
+                addToDo(input);
+            } else if (input.startsWith("event ")) {
+                addEvent(input);
+            } else if (input.startsWith("deadline ")) {
+                addDeadline(input);
             } else if (!input.isEmpty()) {
                 AddTask(input);
             }
@@ -61,6 +67,34 @@ public class Shahzam {
         System.out.println("OK, I've marked this task as not done yet:\n  " + t);
     }
 
+    private void addToDo(String input) {
+        Task t = new ToDo(input.substring(5).trim());
+        TaskList.add(t);
+        System.out.println("Got it. I've added this task:\n  " + t);
+        System.out.println("Now you have " + TaskList.toArray().length + " tasks in your list." );
+    }
+
+    private void addDeadline(String input) {
+        String[] time_str = input.substring(9).split("/by");
+        Task t = new Deadline(time_str[0].trim(), time_str[1].trim());
+        TaskList.add(t);
+        System.out.println("Got it. I've added this task:\n  " + t);
+        System.out.println("Now you have " + TaskList.toArray().length + " tasks in your list." );
+    }
+
+    private void addEvent(String input) {
+        String[] parts = input.substring(6).split("/from");
+        String description = parts[0].trim();
+
+        String[] timeParts = parts[1].split("/to");
+        String fromTime = timeParts[0].trim();
+        String toTime = timeParts[1].trim();
+
+        Task t = new Event(description, fromTime, toTime);
+        TaskList.add(t);
+        System.out.println("Got it. I've added this task:\n  " + t);
+        System.out.println("Now you have " + TaskList.toArray().length + " tasks in your list." );
+    }
     private void PrintList(ArrayList<Task> tasks) {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
