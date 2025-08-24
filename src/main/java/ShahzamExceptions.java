7cd7a475a7fbc0439f7f0b6bc46af1fd514479e1
mihirshah2005 <e1427144@u.nus.edu>
2025-08-24 18:38:08 +0800

    Add new exception

--- src/main/java/ShahzamExceptions.java
+++ src/main/java/ShahzamExceptions.java
@@ -1,30 +1,41 @@
-class UnknownCommandException extends Exception {
+public class ShahzamExceptions extends Exception {
+    public ShahzamExceptions(String message) {
+        super(message);
+    }
+}
+
+class UnknownCommandException extends ShahzamExceptions {
     public UnknownCommandException() {
         super("Sorry but my knowledge does not have such command.");
     }
 }
 
-class EmptyTaskDescriptionException extends Exception {
+class EmptyTaskDescriptionException extends ShahzamExceptions {
     public EmptyTaskDescriptionException(String message) {
         super(message);
     }
 }
 
-class InvalidTaskNumberException extends Exception {
+class InvalidTaskNumberException extends ShahzamExceptions {
     public InvalidTaskNumberException(String message) {
         super(message);
     }
 }
 
-class InvalidDeadlineFormatException extends Exception {
+class InvalidDeadlineFormatException extends ShahzamExceptions {
     public InvalidDeadlineFormatException(String message) {
         super(message);
     }
 }
 
-class InvalidEventFormatException extends Exception {
+class InvalidEventFormatException extends ShahzamExceptions {
     public InvalidEventFormatException(String message) {
         super(message);
     }
 }
 
+class DataIntegrityException extends ShahzamExceptions {
+    public DataIntegrityException() {
+        super("Storage file integrity compromised :(");
+    }
+}
