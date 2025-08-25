5a558162131fa734e113c0a9c8f8726a6bdd04b5
mihirshah2005 <e1427144@u.nus.edu>
2025-08-26 00:24:18 +0800

    Add JavaDoc comments to tasks

--- src/main/java/shahzam/task/Task.java
+++ src/main/java/shahzam/task/Task.java
@@ -1,24 +1,52 @@
 package shahzam.task;
 
+
+/**
+ * Represents a generic task.
+ * A task has a description and a status (done or not done).
+ */
 public class Task {
     private String description;
     private boolean isDone;
+
+    /**
+     * Constructs a new Task.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void MarkDone() {
         this.isDone = true;
     }
+
+    /**
+     * Returns true if the task is marked as done.
+     *
+     * @return True if the task is done, false otherwise.
+     */
     public boolean isDone() {
         return this.isDone;
     }
 
+    /**
+     * Unmarks the task as done.
+     */
     public void UnmarkDone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task (either "X" or " ").
+     *
+     * @return The status icon of the task.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
