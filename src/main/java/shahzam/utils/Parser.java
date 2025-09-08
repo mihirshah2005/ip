d545819b97fbdd1007605ee0ee4df9e5ff4200ba
mihirshah2005 <e1427144@u.nus.edu>
2025-09-08 14:09:23 +0800

    Refactor code for improved quality in TaskList, ToDo and Parser classes

--- src/main/java/shahzam/utils/Parser.java
+++ src/main/java/shahzam/utils/Parser.java
@@ -3,8 +3,23 @@ import shahzam.exception.ShahzamExceptions;
 import shahzam.exception.UnknownCommandException;
 import shahzam.task.TaskList;
 
+/**
+ * Utility class for parsing user input and interpreting commands.
+ * This class is responsible for determining which command the user intends to execute
+ * and returning the corresponding function that will perform the desired action on the TaskList.
+ */
 public class Parser {
 
+    /**
+     * Interprets the user input and returns the corresponding function to be executed on the TaskList.
+     * The function determines the action based on the command and delegates the execution to the
+     * appropriate method in the TaskList class.
+     *
+     * @param input The user input, which represents a command.
+     * @return A CheckedFunction that executes the corresponding TaskList method based on the input.
+     * @throws ShahzamExceptions If the input does not match any known command.
+     * @throws UnknownCommandException If the command is unrecognized.
+     */
     public static CheckedFunction interpretCommand(String input) throws ShahzamExceptions {
         if (input.equals("list")) {
             return TaskList::PrintList;
@@ -28,6 +43,11 @@ public class Parser {
     }
 
 
+    /**
+     * Functional interface representing a function that executes an action on the TaskList.
+     * The function takes a TaskList object and returns a string result, possibly throwing
+     * a ShahzamExceptions.
+     */
     public interface CheckedFunction {
         String execute(TaskList newlist) throws ShahzamExceptions;
     }
